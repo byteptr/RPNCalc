c7b1941dcd5c9da853e1969e33ca0a6e28acd666
raul <ralvarezb78@gmail.com>
2021-09-09 06:25:24 +0200

    added operators file

---
+++ src/operators.py
@@ -0,0 +1,84 @@
+import rpn_calc
+
+class core_operators: 
+    def __init__(self):
+        self.__operators = dict()
+
+    def add(self): 
+        pass
+
+    def sub(self):
+        pass
+
+    def mul(self):
+        pass
+
+    def div(self):
+        pass
+
+    def mod(self):
+        pass
+
+    def sqrt(self):
+        pass
+
+    def pow(self):
+        pass
+
+    def exp(self):
+        pass
+
+    def ln(self):
+        pass
+
+    def log10(self):
+        pass
+
+    def log2(self):
+        pass
+
+    def logab(self):
+        pass
+
+    def sin(self):
+        pass
+
+    def cos(self):
+        pass
+
+    def tan(self):
+        pass
+
+    def asin(self):
+        pass
+
+    def acos(self):
+        pass
+
+    def atan(self):
+        pass
+
+    def sinh(self):
+        pass
+
+    def cosh(self):
+        pass
+
+    def tanh(self):
+        pass
+
+    def asinh(self):
+        pass
+
+    def acosh(self):
+        pass
+
+    def atanh(self):
+        pass
+
+    def pi(self):
+        pass
+
+    def e(self): 
+        pass
+
